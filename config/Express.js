d26a1775436ae4bbf21eab63f2474a49201c8ca2
UlyaMedina28 <ulyakaiforever@gmail.com>
2023-10-11 22:21:13 +0700

    new

--- config/Express.js
+++ config/Express.js
@@ -19,24 +19,24 @@ db.connect((err) => {
   console.log('Connected to MySQL as ID ' + db.threadId);
 });
 
-// Definisikan endpoint atau rute untuk website Anda
+
 app.get('/', (req, res) => {
-  // Lakukan kueri ke database
+
   db.query('SELECT * FROM users', (error, results, fields) => {
     if (error) {
       console.error('Error querying database: ' + error);
       res.status(500).json({ error: 'Internal server error' });
       return;
     }
-    // Gunakan data dari database untuk merender halaman website Anda
+    
     res.json(results);
   });
 });
 
-// Port yang akan digunakan oleh server
+
 const port = 3000;
 
-// Jalankan server
+
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:3000`);
 });
