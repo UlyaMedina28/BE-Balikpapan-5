968c59b6fb872e492caa0201887fd7708cf3dc9e
UlyaMedina28 <ulyakaiforever@gmail.com>
2023-10-11 22:01:56 +0700

    ...

--- nodemon/app.js
+++ nodemon/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // Import modul path untuk mengelola path file
 
 const routes = require('./routes');
 
@@ -8,14 +9,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const router = express.Router();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Menggunakan path untuk mengelola path file
 app.get('/', (req, res) => {
-  res.sendFile(project-be-bookstore + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 routes.forEach((route) => app.use(route));
@@ -23,3 +23,4 @@ routes.forEach((route) => app.use(route));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
